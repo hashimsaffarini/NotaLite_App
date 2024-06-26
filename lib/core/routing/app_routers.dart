8f481f94e76f4d31d14fc8ca4ce1feebd9952e35
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-26 11:35:55 +0300

    first commit

---
+++ lib/core/routing/app_routers.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/core/routing/routes.dart';
+import 'package:to_do/features/onboarding/views/screens/onboarding_screen.dart';
+
+class AppRouter {
+  static Route? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text(
+                'No route defined for ${settings.name}',
+              ),
+            ),
+          ),
+        );
+    }
+  }
+}
